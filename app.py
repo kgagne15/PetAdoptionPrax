224e34f8718ace4690dc62162ee4ab19559bf10c
kgagne15 <kristinagagne@ymail.com>
2022-07-28 17:08:56 -0400

    add form functionality added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from time import pthread_getcpuclockid
 from flask import Flask, request, render_template,  redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db,  connect_db, Pet
@@ -23,3 +24,20 @@ def list_all_pets():
 def add_pet():
     form = AddPetForm()
     return render_template('add_pet_form.html', form=form)
+
+@app.route('/add', methods=["POST"])
+def post_pet():
+    form = AddPetForm()
+    if form.validate_on_submit(): 
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data or None
+        age = form.age.data or None
+        notes = form.notes.data or None
+        new_pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes, available=True)
+        db.session.add(new_pet)
+        db.session.commit()
+        flash(f"{name} is now listed for adoption!")
+        return redirect('/add')
+    else: 
+        return render_template('add_pet_form.html', form=form)
